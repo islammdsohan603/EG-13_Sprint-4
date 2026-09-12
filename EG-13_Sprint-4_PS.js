b15cba3dc1175ca67be801d8bb89840164664f2c
islammdsohan603 <islammdsohan603@gmail.com>
2026-09-12 10:12:30 +0600

    Isomporphic String

---
+++ EG-13_Sprint-4_PS.js
@@ -0,0 +1,30 @@
+// Isomporphic String
+
+var isIsomorphic = function (s, t) {
+  if (s.length !== t.length) {
+    return false;
+  }
+
+  const mapST = {};
+  const mapTS = {};
+
+  for (let i = 0; i < s.length; i++) {
+    const charS = s[i];
+    const charT = t[i];
+
+    if (mapST[charS] && mapST[charS] !== charT) {
+      return false;
+    }
+
+    if (mapTS[charT] && mapTS[charT] !== charS) {
+      return false;
+    }
+
+    mapST[charS] = charT;
+    mapTS[charT] = charS;
+  }
+
+  return true;
+};
+
+console.log(isIsomorphic("egg", "add"));

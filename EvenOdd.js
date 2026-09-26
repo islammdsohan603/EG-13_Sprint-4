790ba066323c4cc5772fb051c88cf04f33aacd32
islammdsohan603 <islammdsohan603@gmail.com>
2026-09-26 08:23:31 +0600

    concat method

--- EvenOdd.js
+++ EvenOdd.js
@@ -1,10 +1,8 @@
-// find() ব্যবহার করে নির্দিষ্ট id-এর user
+// concat() method
 
-const users = [
-  { id: 1, name: "Sohan" },
-  { id: 2, name: "Rahim" },
-  { id: 3, name: "Karim" },
-];
+const arr1 = [1, 2];
+const arr2 = [3, 4];
 
-const user = users.find((user) => user.id === 3);
-console.log(user);
+const result = arr1.concat(arr2);
+
+console.log(result);
